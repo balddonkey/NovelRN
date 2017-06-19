dcc68b7703c9191797412ba0cc0288ca18f1d97b
老马 <huiliu@huiliudeiMac.local>
2017-06-19 15:02:02 +0800

    first commit

---
+++ Library/Reader.js
@@ -0,0 +1,18 @@
+
+
+class Reader extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+        this.actions = [
+            "search"
+        ];
+    }
+}
+const initState = {};
+const actions = [
+    "search"
+]
