70af1d5371585924ebdf9c0c9a41522d0f2ec835
老马 <huiliu@huiliudeiMac.local>
2017-06-28 18:39:09 +0800

    auto commit

--- ZZLibrary/Agent/Biquge/fetcher.js
+++ ZZLibrary/Agent/Biquge/fetcher.js
@@ -6,16 +6,8 @@ const SearchURL = 'http://zhannei.baidu.com/cse/search';
 let Fetcher = {
 
     recommand: function (cb) {
-        fetch('http://gank.io/api/search/query/listview/category/福利/count/10/page/1', {
-            method: 'GET',
-            headers: {
-
-                'Accept': 'application/xml',
-                'Authorization': 'Bearer ' + this.authToken,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
-            })
-            .then((response) => response.html())
+        fetch('http://gank.io/api/search/query/listview/category/福利/count/10/page/1')
+            .then((response) => response.text())
             .then((responseText) => {
                 console.log(responseText);
             })
